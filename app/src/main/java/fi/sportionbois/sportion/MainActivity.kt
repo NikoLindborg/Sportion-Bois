e4d5e720495b29073174888d603c348dac57afb0
aleksikosonen <kososenaleksi@gmail.com>
2022-02-10 13:24:55 +0200

    Navigation basics

--- app/src/main/java/fi/sportionbois/sportion/MainActivity.kt
+++ app/src/main/java/fi/sportionbois/sportion/MainActivity.kt
@@ -3,11 +3,30 @@ package fi.sportionbois.sportion
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import fi.sportionbois.sportion.navigation.BottomNavItem
 import fi.sportionbois.sportion.ui.theme.SportionTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,9 +34,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SportionTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -25,14 +43,127 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Home(name: String) {
+    Text(text = name, color = Color.White)
 }
+
+@Composable
+fun StartTracking(name: String) {
+    Text(text = name, color = Color.White)
+}
+
+@Composable
+fun Settings(name: String) {
+    Text(text = name, color = Color.White)
+}
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(
+        topBar = { TopBar() },
+        bottomBar = { BottomNavigation(navController) }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .background(Color(0xFF263D42)),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            NavigationGraph(navController = navController)
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SportionTheme {
-        Greeting("Android")
+        Home("Android")
+    }
+}
+
+@Composable
+fun TopBar() {
+    TopAppBar(
+        backgroundColor = Color(0xFF263D42),
+        contentColor = Color(0xFFCCDBDC),
+        title = { Text(stringResource(id = R.string.app_name)) },
+        navigationIcon = {
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.Menu, contentDescription = null)
+            }
+        },
+        actions = {
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.Search, contentDescription = "Localized description")
+            }
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(
+                    Icons.Filled.MoreVert,
+                    contentDescription = "Localized description"
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun NavigationGraph(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = BottomNavItem.Home.screen_route) {
+        composable(BottomNavItem.Home.screen_route) {
+            Home(name = "Home")
+        }
+        composable(BottomNavItem.StartTracking.screen_route) {
+            StartTracking(name = "Start tracking")
+        }
+        composable(BottomNavItem.Settings.screen_route) {
+            Settings(name = "Settings")
+        }
+    }
+}
+
+@Composable
+fun BottomNavigation(navController: NavController) {
+    val items = listOf(
+        BottomNavItem.Home,
+        BottomNavItem.StartTracking,
+        BottomNavItem.Settings,
+    )
+    BottomNavigation(
+        backgroundColor = Color(0xFF263D42),
+        contentColor = Color.White
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = { Icon(imageVector = item.icon, contentDescription = item.title) },
+                label = {
+                    Text(
+                        text = item.title,
+                        fontSize = 9.sp
+                    )
+                },
+                selectedContentColor = Color(0xFFCCDBDC),
+                unselectedContentColor = Color(0xFFCCDBDC).copy(0.4f),
+                alwaysShowLabel = true,
+                selected = currentRoute == item.screen_route,
+                onClick = {
+                    navController.navigate(item.screen_route) {
+                        navController.graph.startDestinationRoute?.let { screen_route ->
+                            popUpTo(screen_route) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
     }
 }
---
+++ app/src/main/java/fi/sportionbois/sportion/navigation/BottomNavItem.kt
@@ -0,0 +1,13 @@
+package fi.sportionbois.sportion.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomNavItem(var title: String, var icon: ImageVector, var screen_route: String) {
+    object Home : BottomNavItem("Home", Icons.Filled.Home, "home")
+    object StartTracking : BottomNavItem("Start Tracking", Icons.Filled.Add, "start_tracking")
+    object Settings : BottomNavItem("Settings", Icons.Filled.Settings, "settings")
+}
